681098608f8f9ca2199ff9ad6d31cd85a7640c3f
JromarioSs <jromario2014@gmail.com>
2025-03-08 21:01:12 -0300

    feat: add button do contrato

--- src/components/ModalBuy/index.tsx
+++ src/components/ModalBuy/index.tsx
@@ -6,10 +6,21 @@ import { QRCodeSVG } from 'qrcode.react';
 export const ModalBuy = () => {
   const { onSetOpenModal } = useMain();
   const KEY_USER = "0x085e8d97dfb6e742895fdcd78765139e055d1a42";
+  const KEY_CONTRACT = "0x2317f7B16bB564f2AD99B1B56671630C0A9c6c57";
 
 
   const handleCopy = () => {
     navigator.clipboard.writeText(KEY_USER)
+      .then(() => {
+        alert("QR Code copiado com sucesso!");
+      })
+      .catch(() => {
+        alert("Falhar ao copiar o QR Code tente novamente mais tarde!");
+      });
+  };
+
+  const handleCopyContract = () => {
+    navigator.clipboard.writeText(KEY_CONTRACT)
       .then(() => {
         alert("QR Code copiado com sucesso!");
       })
@@ -32,7 +43,10 @@ export const ModalBuy = () => {
           <p className="text-xl font-medium md:text-2xl">1 OVO = 100 DYNOCOINS</p>
         </div>
 
-        <S.ButtonCopy onClick={handleCopy}>COPIAR</S.ButtonCopy>
+        <div className='flex flex-col gap-4'>
+          <S.ButtonCopy onClick={handleCopy}>COPIAR</S.ButtonCopy>
+          <S.ButtonCopy onClick={handleCopyContract}>CONTRATO</S.ButtonCopy>
+        </div>
       </S.Main>
     </S.Container>
   )
